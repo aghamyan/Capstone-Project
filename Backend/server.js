38d773a6d1f3f10870385cc67407a0ceddab8a33
aghamyan <Arthur_aghamyan@edu.aua.am>
2025-12-15 19:21:47 +0400

    Grig's Help

--- Backend/server.js
+++ Backend/server.js
@@ -39,7 +39,7 @@ const PORT = process.env.PORT || 4000;
 
 // === Middlewares ===
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }))
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 // === Health Check ===
